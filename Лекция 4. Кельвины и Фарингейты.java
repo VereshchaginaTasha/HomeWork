8e0d52007afb9cccfd08b55f9ad4013290148380
VereshchaginaTasha <116460697+VereshchaginaTasha@users.noreply.github.com>
2022-11-04 17:23:25 +0300

    Add files via upload

---
+++ Лекция 4. Кельвины и Фарингейты.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      
+      double tC = 36.6;
+      
+      System.out.println(tC*1.8+32); // tC в Фарингейтах 
+      System.out.println(tC+273.15); // tC в Кельвинах
+      
+  }
+}
