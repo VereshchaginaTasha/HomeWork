b156e9f20113d3f266c4153b0a2e7a890bf8b9d2
VereshchaginaTasha <116460697+VereshchaginaTasha@users.noreply.github.com>
2022-11-05 18:40:25 +0300

    Add files via upload

---
+++ Лекция 4. Булева алгебра. Логические операции.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+    
+    int a = 10;
+    int b = 20;
+    int c = 5;
+    
+    boolean result1 = (b<c&&b>a);
+    boolean result2 = (a>c&&a<b); 
+   
+   System.out.println(result1);
+   System.out.println(result2);
+  }
+}
