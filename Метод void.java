87b3d26c6111c4f2f156021e6a1ddf51780fd08e
VereshchaginaTasha <116460697+VereshchaginaTasha@users.noreply.github.com>
2022-11-12 15:20:44 +0300

    Add files via upload

---
+++ Метод void.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+    public static void print(int a, int b) {
+      if (a>b) {
+       System.out.println(a);
+      }
+      else 
+    System.out.println(b);
+    }
+   
+   
+    public static void main(String[] args) {
+       
+       print(6,4);
+     
+     
+  }
+}
