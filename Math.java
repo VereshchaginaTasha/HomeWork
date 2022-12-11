5f12532460d9f10b34c698843f8e3b18e1a8418d
VereshchaginaTasha <116460697+VereshchaginaTasha@users.noreply.github.com>
2022-12-11 19:47:14 +0300

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      Scanner Scanner = new Scanner(System.in);
+
+      
+      System.out.println(Math.sqrt (Math.PI));
+      System.out.println(Math.sqrt (Math.E));
+      System.out.println(Math.sqrt (2.718281));
+  
+    }
+}
